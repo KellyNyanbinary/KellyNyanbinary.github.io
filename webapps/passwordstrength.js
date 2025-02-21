2ef7737c353bd706d3562821e738087cc81075f4
KellyNyanbinary <43101126+KellyNyanbinary@users.noreply.github.com>
2025-02-21 05:02:03 -0700

    Add character set display
    Improve description

--- webapps/passwordstrength.js
+++ webapps/passwordstrength.js
@@ -2,48 +2,74 @@ document.addEventListener("DOMContentLoaded", calculateStrength);
 
 function calculateStrength() {
     const passwordInput = document.getElementById("password");
+    const characterMessage = document.getElementById("password-characters");
     const strengthMessage = document.getElementById("password-strength");
 
     passwordInput.addEventListener("input", function() {
         const password = passwordInput.value;
         const baseRatePerYear = 500000 * 3600 * 24 * 365.25  // guesses per second, taken from https://openbenchmarking.org/test/pts/john-the-ripper for a single high end server in 2025
 
+        let rangeSize = 0
+        let characterText = ""
+
+        if (/[a-z]/g.test(password)) {
+            rangeSize += 26
+            characterText += "Lowercase letters &#9989;<br>"
+        } else
+            characterText += "Lowercase letters &#10060;<br>"
+
+        if (/[A-Z]/g.test(password)) {
+            rangeSize += 26
+            characterText += "Uppercase letters &#9989;<br>"
+        } else
+            characterText += "Uppercase letters &#10060;<br>"
+
+        if (/[0-9]/g.test(password)) {
+            rangeSize += 10
+            characterText += "Numerical digits &#9989;<br>"
+        } else
+            characterText += "Numerical digits &#10060;<br>"
+
+        if (/[\x20-\x2F]|[\x3A-\x40]|[\x5B-\x60]|[\x7B-\x7E]/g.test(password)) {
+            rangeSize += 33
+            characterText += "Special characters &#9989;<br>"
+        } else
+            characterText += "Special characters &#10060;<br>"
+
+        characterMessage.innerHTML = characterText
+
         // Validate password characters.
         if (password.length == 0) {
             strengthMessage.innerHTML = "Enter a password to see expected time to guess."
-        } else if (!isValidPasswordChars(password)) {
+            characterMessage.innerHTML = characterText
+            return
+        }
+
+        // Validate password characters.
+        if (!isValidPasswordChars(password)) {
             strengthMessage.innerHTML = "Password must contain only ASCII letters and punctuation.";
+            characterMessage.innerHTML = characterText
+            return
+        }
+
+        const possibilities = rangeSize ** password.length
+        const entropy = password.length * Math.log2(rangeSize)
+
+        const possibilitiesText = "There are " + possibilities.toExponential(3) + " possibilities."
+        const rangeSizeText = "The range size is " + rangeSize + " characters."
+
+        // This is Wolfram Alpha's result.
+        const yearsToGuess = 2.8854 * Math.log(
+            (0.3465735 * possibilities) / baseRatePerYear + 2
+        ) - 2
+
+        const yearText = yearsToGuess.toFixed(0) == "1" ? "year" : "years"
+        const yearsToGuessText = "It is expected to take " + yearsToGuess.toFixed(0) + " " + yearText + " to guess this password."
+
+        if (yearsToGuess < 0) {
+            strengthMessage.innerHTML = rangeSizeText + "<br>" + possibilitiesText + "<br>" + "Password is too short and can be guessed in under a year."
         } else {
-            let rangeSize = 0
-            if (/[a-z]/g.test(password))
-                rangeSize += 26
-            if (/[A-Z]/g.test(password))
-                rangeSize += 26
-            if (/[0-9]/g.test(password))
-                rangeSize += 10
-            if (/[\x20-\x2F]|[\x3A-\x40]|[\x5B-\x60]|[\x7B-\x7E]/g.test(password))
-                rangeSize += 33
-
-            const possibilities = rangeSize ** password.length
-            const entropy = password.length * Math.log2(rangeSize)
-
-            const possibilitiesText = "There are " + possibilities.toExponential(3) + " possibilities."
-            const rangeSizeText = "The range size is " + rangeSize + " characters."
-
-            // This is Wolfram Alpha's result.
-            const yearsToGuess = 2 * 1.4427 * Math.log(
-                (0.693146 * possibilities / 2) / baseRatePerYear
-            ) - 2
-
-            const yearText = yearsToGuess.toFixed(0) == "1" ? "year" : "years"
-            const yearsToGuessText = "It is expected to take " + yearsToGuess.toFixed(0) + " " + yearText + " to guess this password."
-
-            if (yearsToGuess < 0) {
-                strengthMessage.innerHTML = rangeSizeText + "<br>" + possibilitiesText + "<br>" + "Password is too short and can be guessed in under a year."
-            } else {
-
-                strengthMessage.innerHTML = rangeSizeText + "<br>" + possibilitiesText + "<br>" + yearsToGuessText
-            }
+            strengthMessage.innerHTML = rangeSizeText + "<br>" + possibilitiesText + "<br>" + yearsToGuessText
         }
     });
 }
